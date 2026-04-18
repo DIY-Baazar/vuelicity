0fd1d44e0da21d7ae7b23d72c6360132b78b4f9d
Purbayan Chowdhury <pur.cho.99@gmail.com>
2026-04-18 17:00:43 +0530

    fix: adjust slot v-if conditions for arrow and home icons in PubBreadcrumbItem

--- src/components/PubBreadcrumb/PubBreadcrumbItem.vue
+++ src/components/PubBreadcrumb/PubBreadcrumbItem.vue
@@ -16,12 +16,12 @@ const { breadcrumbItemClasses } = useBreadcrumbItemClasses(toRefs(props));
 
 <template>
     <li class="inline-flex items-center">
-        <slot name="arrow-icon">
-            <pub-icon name="chevron-right" type="outline" :stroke-width="2" v-if="!props.home" size="xs" />
+        <slot name="arrow-icon" v-if="!props.home">
+            <pub-icon name="chevron-right" type="outline" :stroke-width="2" size="xs" />
         </slot>
         <component :is="componentName" :class="['pub-breadcrumb-item', breadcrumbItemClasses]" :href="href">
-            <slot name="home-icon">
-                <pub-icon name="home" v-if="props.home" size="xs" />
+            <slot name="home-icon" v-if="props.home">
+                <pub-icon name="home" size="xs" />
             </slot>
             <slot name="default" />
         </component>
